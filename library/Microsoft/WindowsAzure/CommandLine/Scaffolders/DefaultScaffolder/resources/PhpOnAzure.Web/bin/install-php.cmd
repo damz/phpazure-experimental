fea45c273a0edb7b93575e0b7ee361ddba4c1030
SND\maartenba_cp <SND\maartenba_cp@ffd33b8c-2492-42e0-bdc5-587b920b7d6d>
2011-05-31 13:08:54 +0000

    Updated default scaffolder to have PHP53, Wincache and SQL Server Driver for PHP installed at boot.
    git-svn-id: https://PHPAzure.svn.codeplex.com/svn/trunk@62718 ffd33b8c-2492-42e0-bdc5-587b920b7d6d

--- library/Microsoft/WindowsAzure/CommandLine/Scaffolders/DefaultScaffolder/resources/PhpOnAzure.Web/bin/install-php.cmd
+++ library/Microsoft/WindowsAzure/CommandLine/Scaffolders/DefaultScaffolder/resources/PhpOnAzure.Web/bin/install-php.cmd
@@ -7,6 +7,9 @@ cd ..
 
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d "%~dp0appdata" /f
 "..\resources\WebPICmdLine\webpicmdline" /Products:PHP53 /AcceptEula  >> ..\startup-tasks-log.txt 2>>..\startup-tasks-error-log.txt
+"..\resources\WebPICmdLine\webpicmdline" /Products:SQLNativeClient /AcceptEula  >> ..\startup-tasks-log.txt 2>>..\startup-tasks-error-log.txt
+"..\resources\WebPICmdLine\webpicmdline" /Products:SQLDriverPHP53IIS /AcceptEula  >> ..\startup-tasks-log.txt 2>>..\startup-tasks-error-log.txt
+"..\resources\WebPICmdLine\webpicmdline" /Products:WinCache53 /AcceptEula  >> ..\startup-tasks-log.txt 2>>..\startup-tasks-error-log.txt
 reg add "hku\.default\software\microsoft\windows\currentversion\explorer\user shell folders" /v "Local AppData" /t REG_EXPAND_SZ /d %%USERPROFILE%%\AppData\Local /f
 
 ECHO "Completed PHP installation." >> ..\startup-tasks-log.txt
